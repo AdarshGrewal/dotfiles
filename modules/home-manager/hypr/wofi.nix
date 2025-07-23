c7bc6a5873325136e187d5e095f9580fd4bdc036
AdarshGrewal <adarshgrewal@gmail.com>
2025-07-24 01:22:08 +0530

    dots: hypr: rice wofi

--- modules/home-manager/hypr/wofi.nix
+++ modules/home-manager/hypr/wofi.nix
@@ -7,5 +7,24 @@
 }:
 
 {
-  programs.wofi.enable = true;
+  home.packages = with pkgs; [
+    nerd-fonts.inconsolata
+  ];
+  programs.wofi = {
+    enable = true;
+    settings = {
+      show = "drun";
+      width = "750";
+      height = "400";
+      always_parse_args = true;
+      show_all = false;
+      term = "kitty";
+      hide_scroll = true;
+      print_command = true;
+      insensitive = true;
+      prompt = "";
+      columns = "2";
+    };
+    style = builtins.readFile ./wofi.css;
+  };
 }
