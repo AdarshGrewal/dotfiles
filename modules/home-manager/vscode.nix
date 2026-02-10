8fabb8eca5c87cffbea6d0b9a6f3bb3cc31ee46f
AdarshGrewal <adarshgrewal@gmail.com>
2026-02-10 22:48:52 +0530

    dots: home: vscode: Use firacode nerd

--- modules/home-manager/vscode.nix
+++ modules/home-manager/vscode.nix
@@ -9,10 +9,22 @@
 }:
 
 {
+  home.packages = with pkgs; [
+    nerd-fonts.fira-code
+  ];
+
   programs.vscode = {
     enable = true;
-    profiles.default.extensions = with pkgs.vscode-extensions; [
-      jnoortheen.nix-ide
-    ];
+    profiles.default = {
+      userSettings = {
+        "editor.fontSize" = 14;
+        "editor.fontFamily" = "'FiraCode Nerd Font', 'monospace', monospace";
+        "terminal.integrated.fontSize" = 14;
+        "terminal.integrated.fontFamily" = "'FiraCode Nerd Font Mono', 'monospace', monospace";
+      };
+      extensions = with pkgs.vscode-extensions; [
+        jnoortheen.nix-ide
+      ];
+    };
   };
 }
