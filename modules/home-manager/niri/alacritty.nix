93759c9e6c7cf62a90f28810e836aa2f735e00b9
AdarshGrewal <adarshgrewal@gmail.com>
2026-02-11 01:30:24 +0530

    dots: home: alacritty: Add padding

--- modules/home-manager/niri/alacritty.nix
+++ modules/home-manager/niri/alacritty.nix
@@ -11,5 +11,13 @@
   programs.alacritty = {
     enable = true;
     theme = "catppuccin_mocha";
+    settings = {
+      window = {
+        padding = {
+          x = 10;
+          y = 10;
+        };
+      };
+    };
   };
 }
